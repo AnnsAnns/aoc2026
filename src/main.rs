b289eb3897ba5f9c1cd237a6843810056fb28d5b
AnnsAnn <git@annsann.eu>
2025-12-06 11:10:12 +0100

    Redo main so I can make it open source

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,68 @@
+use std::env;
 
 mod day_five;
 mod day_four;
 mod day_one;
+mod day_six;
 mod day_three;
 mod day_two;
 mod utils;
-mod day_six;
 
 fn main() {
-    // let day_one_input = include_str!("../inputs/day_one.txt");
-    // let mut day_one_solver = day_one::DayOne::new(&day_one_input);
-    // day_one_solver.calculate_position();
-    // println!("Day One: {:?}", day_one_solver.get_number_of_zeroes());
+    let args: Vec<String> = env::args().collect();
 
-    // let day_two_input = include_str!("../inputs/day_two.txt");
-    // let mut day_two_solver = day_two::DayTwo::new(&day_two_input);
-    // println!("Day Two: {:?}", day_two_solver);
-    // day_two_solver.find_wrong_ids();
-    // day_two_solver.print_wrong_ids();
-    // println!("Day Two Answer: {:?}", day_two_solver.get_total_wrong_ids());
-    //
+    if args.len() < 2 {
+        eprintln!("Usage: {} <day_number>", args[0]);
+        std::process::exit(1);
+    }
 
-    // let mut day_four = DayFour::new(include_str!("../inputs/day_four.txt"));
-    // day_four.repeat_until_no_changes();
-    // println!("Day Four Answer: {:?}", day_four.get_total());
+    let day = &args[1];
 
-    // let mut day_three = DayThree::new(include_str!("../inputs/day_three.txt"));
-    // day_three.process_input(12);
-    // println!("Day Three Answer Part 1: {:?}", day_three.get_total());
-    //
-    // let mut day_five = day_five::DayFive::new(include_str!("../inputs/day_five.txt"));
-    // day_five.calc_total();
-    // println!("Day Five Answer: {:?}", day_five.get_total_valid());
-    // day_five.calc_covered_ranges();
-    // println!(
-    //     "Valid {}",
-    //     day_five.calculate_all_valid_ids_from_covered_range()
-    // )
-
-    let mut day_six = day_six::DaySix::new(day_six::GRID_DATA);
-    day_six.process_grid();
+    match day.as_str() {
+        "1" => {
+            let day_one_input = utils::file_to_string("./inputs/day_one.txt");
+            let mut day_one_solver = day_one::DayOne::new(&day_one_input);
+            day_one_solver.calculate_position();
+            println!("Day One: {:?}", day_one_solver.get_number_of_zeroes());
+        }
+        "2" => {
+            let day_two_input = utils::file_to_string("./inputs/day_two.txt");
+            let mut day_two_solver = day_two::DayTwo::new(&day_two_input);
+            day_two_solver.find_wrong_ids();
+            day_two_solver.print_wrong_ids();
+            println!("Day Two Answer: {:?}", day_two_solver.get_total_wrong_ids());
+        }
+        "3" => {
+            let mut day_three_input = utils::file_to_string("./inputs/day_three.txt");
+            let mut day_three = day_three::DayThree::new(&day_three_input);
+            day_three.process_input(12);
+            println!("Day Three Answer Part 1: {:?}", day_three.get_total());
+        }
+        "4" => {
+            let mut day_four_input = utils::file_to_string("./inputs/day_four.txt");
+            let mut day_four = day_four::DayFour::new(&day_four_input);
+            day_four.repeat_until_no_changes();
+            println!("Day Four Answer: {:?}", day_four.get_total());
+        }
+        "5" => {
+            let mut day_five_input = utils::file_to_string("./inputs/day_five.txt");
+            let mut day_five = day_five::DayFive::new(&day_five_input);
+            day_five.calc_total();
+            println!("Day Five Answer: {:?}", day_five.get_total_valid());
+            day_five.calc_covered_ranges();
+            println!(
+                "Valid {}",
+                day_five.calculate_all_valid_ids_from_covered_range()
+            );
+        }
+        "6" => {
+            let mut day_six_input = utils::file_to_string("./inputs/day_six.txt");
+            let mut day_six = day_six::DaySix::new(&day_six_input);
+            day_six.process_grid();
+        }
+        _ => {
+            eprintln!("Invalid day: {}. Please choose 1-6.", day);
+            std::process::exit(1);
+        }
+    }
 }
